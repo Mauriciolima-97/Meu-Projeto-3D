9061011808606d8e8637f55ed1cc9b0137c67422
Mauriciolima-97 <mauricio_rodrigues2014@hotmail.com>
2026-02-09 20:14:36 -0300

    add correr

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,9 +9,15 @@ public class Player : MonoBehaviour
     public CharacterController characterController; 
     public float speed = 1f;
     public float turnSpeed = 1f; 
-    public float gravity = 9.8f; 
+    public float gravity = 9.8f;
+    public float jumpSpeed = 15f;
 
-    private float vSpeed = 0f; 
+    private float vSpeed = 0f;
+    public KeyCode jumpKeyCode = KeyCode.Space;
+
+    [Header("Run Setup")]
+    public KeyCode keyRun = KeyCode.LeftShift;
+    public float speedRun = 1.5f;
 
     void Update()
     { 
@@ -19,20 +25,32 @@ public class Player : MonoBehaviour
 
         var inputAxisVertical = Input.GetAxis("Vertical");
         var speedVector = transform.forward * inputAxisVertical * speed;
-        
-        vSpeed -= gravity * Time.deltaTime;
-        speedVector.y = vSpeed; 
 
-        characterController.Move(speedVector * Time.deltaTime);
+        if (characterController.isGrounded)
         {
-            if (inputAxisVertical != 0)
-            { 
-                animator.SetBool("Run", true);
-            } 
-            else
-            { 
-                animator.SetBool("Run", false);
+            vSpeed = 0;
+            if (Input.GetKeyDown(jumpKeyCode))
+            {
+                vSpeed = jumpSpeed;
             }
         }
+        
+        vSpeed -= gravity * Time.deltaTime;
+        speedVector.y = vSpeed;
+
+        var isWalking = inputAxisVertical != 0;
+        if (isWalking)
+            if(Input.GetKey(keyRun))
+        {
+                speedVector *= speedRun;
+                animator.speed = speedRun;
+        }
+            else
+            {
+                animator.speed = 1;
+            }
+
+        characterController.Move(speedVector * Time.deltaTime);
+        animator.SetBool("Run", inputAxisVertical != 0);
     }
 }
