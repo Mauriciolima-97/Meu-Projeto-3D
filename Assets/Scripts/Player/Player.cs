a6be5f8cc34a8de28de436cef54cc9ef7189b566
Mauriciolima-97 <mauricio_rodrigues2014@hotmail.com>
2026-02-09 20:51:38 -0300

    add ajustes

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,9 +6,9 @@ public class Player : MonoBehaviour
 {
     public Animator animator;
 
-    public CharacterController characterController; 
+    public CharacterController characterController;
     public float speed = 1f;
-    public float turnSpeed = 1f; 
+    public float turnSpeed = 1f;
     public float gravity = 9.8f;
     public float jumpSpeed = 15f;
 
@@ -20,7 +20,7 @@ public class Player : MonoBehaviour
     public float speedRun = 1.5f;
 
     void Update()
-    { 
+    {
         transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
 
         var inputAxisVertical = Input.GetAxis("Vertical");
@@ -34,17 +34,17 @@ public class Player : MonoBehaviour
                 vSpeed = jumpSpeed;
             }
         }
-        
+
         vSpeed -= gravity * Time.deltaTime;
         speedVector.y = vSpeed;
 
         var isWalking = inputAxisVertical != 0;
         if (isWalking)
-            if(Input.GetKey(keyRun))
-        {
+            if (Input.GetKey(keyRun))
+            {
                 speedVector *= speedRun;
                 animator.speed = speedRun;
-        }
+            }
             else
             {
                 animator.speed = 1;
