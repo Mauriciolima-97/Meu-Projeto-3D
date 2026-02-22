0e0692a05d97c0f384baa9a8fa3e5da6d86dc538
Mauriciolima-97 <mauricio_rodrigues2014@hotmail.com>
2026-02-22 00:39:01 -0300

    add checkpoints funcional

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -86,8 +86,24 @@ public class Player : MonoBehaviour//, IDamageable
             _alive = false;
             animator.SetTrigger("Death");
             colliders.ForEach(i => i.enabled = false);
+
+            Invoke(nameof(Revive), 3f);
         }
 
+    }
+
+    private void Revive()
+    {
+        _alive = true;
+        healthBase.ResetLife();
+        animator.SetTrigger("Revive");
+        Respawn();
+        Invoke(nameof(TurnOnColliders), .1f);
+    }
+
+    private void TurnOnColliders()
+    {
+        colliders.ForEach(i => i.enabled = true);
     }
     public void Damage(HealthBase h)
     {
@@ -99,4 +115,17 @@ public class Player : MonoBehaviour//, IDamageable
         //Damage(damage);
     }
     #endregion
+
+    [NaughtyAttributes.Button]
+    public void Respawn()
+    {
+        if (CheckpointManager.Instance.HasCheckpoint())
+        {
+            Vector3 pos = CheckpointManager.Instance.GetPositionFromLastCheckpoint();
+
+            characterController.enabled = false;
+            transform.position = pos;
+            characterController.enabled = true;
+        }
+    }
 }
